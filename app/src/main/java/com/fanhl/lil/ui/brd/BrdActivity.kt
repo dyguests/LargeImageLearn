bab0f86aec763da73d69c721379c8e968f1abda7
fanhl <dyguests@qq.com>
2018-09-11 21:12:23 +0800

    Init commit.

---
+++ app/src/main/java/com/fanhl/lil/ui/brd/BrdActivity.kt
@@ -0,0 +1,19 @@
+package com.fanhl.lil.ui.brd
+
+import android.graphics.BitmapRegionDecoder
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.fanhl.lil.R
+
+/**
+ * BitmapRegionDecoder
+ */
+class BrdActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_brd)
+
+//BitmapRegionDecoder
+    }
+}
---
+++ app/src/main/java/com/fanhl/lil/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.fanhl.lil.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.fanhl.lil.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
