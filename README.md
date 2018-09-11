2cb88891279c988e19c0cf0aaf41b53a77ca947e
fanhl <dyguests@qq.com>
2018-09-11 21:39:20 +0800

    BitmapRegionDecoder

---
+++ README.md
@@ -0,0 +1,4 @@
+# LargeImageLearn
+
+## Reference
+[Android 高清加载巨图方案 拒绝压缩图片](https://blog.csdn.net/lmj623565791/article/details/49300989)
--- app/src/main/java/com/fanhl/lil/ui/brd/BrdActivity.kt
+++ app/src/main/java/com/fanhl/lil/ui/brd/BrdActivity.kt
@@ -1,9 +1,14 @@
 package com.fanhl.lil.ui.brd
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.BitmapRegionDecoder
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Rect
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.fanhl.lil.R
+import kotlinx.android.synthetic.main.activity_brd.*
+import java.io.IOException
 
 /**
  * BitmapRegionDecoder
@@ -14,6 +19,28 @@ class BrdActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_brd)
 
-//BitmapRegionDecoder
+        try {
+            val ins = assets.open("large_img.jpg")
+
+            //获得图片的宽、高
+            val tmpOptions = BitmapFactory.Options().apply {
+                inJustDecodeBounds = true
+            }
+            BitmapFactory.decodeStream(ins, null, tmpOptions)
+            val width = tmpOptions.outWidth
+            val height = tmpOptions.outHeight
+
+            //设置显示图片的中心区域
+            val bitmapRegionDecoder = BitmapRegionDecoder.newInstance(ins, false)
+            val options = BitmapFactory.Options().apply {
+                inPreferredConfig = Bitmap.Config.RGB_565
+            }
+
+            val bitmap = bitmapRegionDecoder.decodeRegion(Rect(width / 2 - 100, height / 2 - 100, width / 2 + 100, height / 2 + 100), options)
+
+            img_large.setImageBitmap(bitmap)
+        } catch (e: IOException) {
+        } finally {
+        }
     }
 }
