6452f524443ecbeb2b0e405162f6bc6a085fe373
fanhl <dyguests@qq.com>
2018-09-13 22:34:10 +0800

    compress

---
+++ app/src/main/java/com/fanhl/lil/ui/compress/CompressActivity.kt
@@ -0,0 +1,15 @@
+package com.fanhl.lil.ui.compress
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.fanhl.lil.R
+
+class CompressActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compress)
+
+
+    }
+}
