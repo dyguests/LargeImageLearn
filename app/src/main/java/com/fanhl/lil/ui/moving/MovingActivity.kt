d4ddccfcd16cc68ac5e8cab2025265dc7b4b3e32
fanhl <dyguests@qq.com>
2018-09-15 18:31:17 +0800

    动图加载

---
+++ app/src/main/java/com/fanhl/lil/ui/moving/MovingActivity.kt
@@ -0,0 +1,16 @@
+package com.fanhl.lil.ui.moving
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.fanhl.lil.R
+
+/**
+ * 动图加载
+ */
+class MovingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_moving)
+    }
+}
