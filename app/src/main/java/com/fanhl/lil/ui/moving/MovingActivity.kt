80ecf7dbec5d344797ffed0b63aec6036b235815
fanhl <dyguests@qq.com>
2018-09-16 11:24:37 +0800

    add glide

--- app/src/main/java/com/fanhl/lil/ui/moving/MovingActivity.kt
+++ app/src/main/java/com/fanhl/lil/ui/moving/MovingActivity.kt
@@ -3,6 +3,7 @@ package com.fanhl.lil.ui.moving
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.fanhl.lil.R
+import kotlinx.android.synthetic.main.activity_moving.*
 
 /**
  * 动图加载
@@ -12,5 +13,7 @@ class MovingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_moving)
+
+        img_1.setImageResource(R.drawable.gif_large)
     }
 }
