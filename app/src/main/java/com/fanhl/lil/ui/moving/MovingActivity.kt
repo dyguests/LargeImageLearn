6a6802e3a612e052876cfb47855341781528a669
fanhl <dyguests@qq.com>
2018-09-16 13:51:25 +0800

    MovingActivity

--- app/src/main/java/com/fanhl/lil/ui/moving/MovingActivity.kt
+++ app/src/main/java/com/fanhl/lil/ui/moving/MovingActivity.kt
@@ -2,6 +2,7 @@ package com.fanhl.lil.ui.moving
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.fanhl.lil.R
 import kotlinx.android.synthetic.main.activity_moving.*
 
@@ -14,6 +15,10 @@ class MovingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_moving)
 
-        img_1.setImageResource(R.drawable.gif_large)
+        fab.setOnClickListener {
+            Glide.with(img_1)
+                    .load(R.drawable.gif_large)
+                    .into(img_1)
+        }
     }
 }
